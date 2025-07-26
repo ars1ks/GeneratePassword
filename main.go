4ee04c4414d7af16e942eb8b509116b597a724e2
ars1ks <arszxc0101@yandex.ru>
2025-07-26 19:28:08 +0000

    Структура

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"encoding/json"
 	"github.com/fatih/color"
 )
+
+type message struct{
+	Suite string `json:"suite"`
+	Password strint `json:"password"`
+}
+
+func key(){
+
+}
+
+func encoder(){
+
+}
+
+func decoder(){
+
+
+}
 
 func main() {
 	color.Green(`
@@ -25,4 +46,20 @@ color.Green(`
 	2.Придумать новый пароль;
 	3.Сгенирировать Ключ;
 	4.Выход`)
+
+	var cout int
+	fmt.Scanln(&cout)
+	
+	switch cout{ 
+	case 1:
+		fmt.Println("1")
+	case 2:
+		fmt.Println("2")
+	case 3:
+		fmt.Println("3")
+	case 4:
+		return
+	default:
+			fmt.Println("Нет такого выбора")
+	}
 }
